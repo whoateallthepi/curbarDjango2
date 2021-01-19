ad9790b8714c1be1fa1ba9a9dade85bd91f5dc09
Tom Cooper <tom@peakmedia.info>
2021-01-19 10:16:56 +0000

    Minor formatting improvements

--- curbargap/blog/templatetags/blog_tags.py
+++ curbargap/blog/templatetags/blog_tags.py
@@ -5,5 +5,9 @@ register = template.Library()
 
 @register.inclusion_tag('blog/post/latest_posts.html') 
 def show_latest_posts(count=5):
+    # always show the 'About 
     latest_posts = Post.published.order_by('-publish') [:count]
-    return {'latest_posts': latest_posts}
+    about_post = Post.published.filter(slug='about-curbargapinfo').first()
+    #breakpoint()
+    return {'latest_posts':latest_posts,
+            'about_post' : about_post}
