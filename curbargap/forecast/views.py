37ccf68ac61835cd94aaa186db2f1f11019b5b73
Tom Cooper <tom@peakmedia.info>
2021-02-02 12:05:54 +0000

    Presentation improvements

--- curbargap/forecast/views.py
+++ curbargap/forecast/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView, View
 from django.utils.timezone import datetime, timedelta
 from django.utils import timezone
-from datetime import date
+from datetime import date, time
 
 from forecast.models import Forecast, TimeSeries, Symbol
 from django.conf import settings
@@ -45,11 +45,16 @@ class SummaryForecastView(View):
     template_name = 'forecast/forecast/summary.html'
     def get(self, request, *args, **kwargs):
 
-        # first get latest forecast object
-        f = Forecast.objects.filter(type=1).latest('forecast_date')
+        # first get latest  hourly forecast object
+        f1 = Forecast.objects.filter(type=1).latest('forecast_date')
+
+        # first get latest  3-hourly forecast object
+
+        f3 = Forecast.objects.filter(type=3).latest('forecast_date')
   
         # then all the TimeSeries - earliest first is the default
-        t =  f.timeseries_set.all()
+        t1 = f1.timeseries_set.all()
+        t3 = f3.timeseries_set.all()
 
         #Generate a list comp of 3 days from now ...  
         td = date.today()
@@ -60,19 +65,26 @@ class SummaryForecastView(View):
             dates.append((td + timedelta(days=d)))
    
         past_hour = timezone.now() + timedelta(hours=-1)
+        ninePM = time(21,00)
+
         
-        # create a list to hold TimeSeries tables  
-        tables = []
+        tables = [] # create a list to hold TimeSeries tables  
+
         for day in dates[0:3]:
-            timeseries = t.filter(series_time__date=day).filter(series_time__gt=past_hour) # exclude expired forecasts
+            timeseries = t1.filter(series_time__date=day).filter(series_time__gt=past_hour) # exclude expired forecasts
+            # if the last of the timeseries is before 9pm - likely this is day 2 
+            # (days go 0,1,2) and the houly forecsts are not a complete day. 
+            # We need to fill in the rest of the day with data from the 3-hourly forecast
+            #
+            last_reading = timeseries[(len(timeseries)-1)].series_time
+            if last_reading.time() < ninePM:
+                timeseries_extra = t3.filter(series_time__date=day).filter(series_time__gt=last_reading)
+                timeseries = timeseries|timeseries_extra
+
             tables.append(TimeSeriesTable(timeseries, orderable=False))                    
 
         # move on to process 3-day forecasts
 
-        f3 = Forecast.objects.filter(type=3).latest('forecast_date')
-
-        # then all the TimeSeries - earliest first is the default
-        t3 =  f3.timeseries_set.all()
         for day in dates [3:]:
             timeseries = t3.filter(series_time__date=day)
             tables.append(TimeSeriesTable(timeseries, orderable=False))
@@ -80,5 +92,5 @@ class SummaryForecastView(View):
 
         return render (request, self.template_name, {'tables': tables,
                                                      'dates': dates,
-                                                     'issued' : f.forecast_date,
-                                                     'place' : f.name })    
+                                                     'issued' : f1.forecast_date,
+                                                     'place' : f1.name })    
